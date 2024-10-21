e3afd309ecfe44e19d8c8a5b19a4222ae4f14c34
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-10-21 16:21:43 +0530

    Time: 31 ms (83.01%) | Memory: 44.8 MB (84.31%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,23 +1,27 @@
 class Solution {
-    public int helper(String s, int i, HashSet<String>set){
-       if(i>=s.length()){
+    public int helper(String s, HashSet<String>set, int i){
+   
+
+     if(i>=s.length()){
         return set.size();
-       }
-        int ans=0;
-        for(int j=i;j<s.length();j++){
-            String a=s.substring(i,j+1);
-            if(set.contains(a)==false){
-                set.add(a);
-                ans=Math.max(ans, helper(s,j+1,set));
-                set.remove(a);
-            }
+     }
+      int ans=0;
+      for(int j=i;j<s.length();j++){
+        String a=s.substring(i,j+1);
+
+        if(set.contains(a)==false){
+            set.add(a);
+            ans=Math.max(ans, helper(s,set,j+1));
+            set.remove(a);
         }
-        return ans;
+      }
+      return ans;
     }
+    
     public int maxUniqueSplit(String s) {
         HashSet<String>set=new HashSet<>();
-        
-       return helper( s,0, set);
-        
+        return helper(s,set ,0);
+
     }
 }
+ 
