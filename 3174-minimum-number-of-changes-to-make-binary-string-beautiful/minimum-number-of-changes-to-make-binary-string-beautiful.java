9d57511a74dbd583f5c4b59d73d275ce12dff3d1
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-11-05 10:57:09 +0530

    Time: 3 ms (97.78%) | Memory: 45.1 MB (12.50%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -8,15 +8,15 @@ class Solution {
              count++;
            }
         }
-        int count1=0;
-        for(int i=s.length()-1;i>=0;i-=2){
-            char ch1=s.charAt(i);
-            char ch2=s.charAt(i-1);
-           if(ch1!=ch2){
-             count1++;
-           }
-        }
-        return Math.min(count,count1);
-
+        // int count1=0;
+        // for(int i=s.length()-1;i>=0;i-=2){
+        //     char ch1=s.charAt(i);
+        //     char ch2=s.charAt(i-1);
+        //    if(ch1!=ch2){
+        //      count1++;
+        //    }
+        // }
+        // return Math.min(count,count1);
+    return count;
     }
 }
