825e89ef1d0909cb57688c50fc44d1d5872fc933
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2024-12-05 16:57:53 +0530

    Time: 12 ms (92.35%) | Memory: 45.6 MB (54.17%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -1,25 +1,3 @@
-// class Solution {
-//     public boolean canChange(String start, String target) {
-//         int i=0;
-//         int j=0;
-//         int n=start.length();
-//         while(i<n || j<n){
-            
-//             while(i<n && start.charAt(i)=='_')i++;
-//             while(j<n && target.charAt(j)=='_')j++;
-//             if(i==n || j==n)return i==n && j==n;
-//             char ch1=start.charAt(i);
-//             char ch2=target.charAt(j);
-//            if(ch1!=ch2)return false;
-//            if(ch1=='L' && i<j)return false;
-//            if(ch1=='R' && i>j)return false;
-//            i++;
-//            j++;
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean canChange(String start, String target) {
         int i=0;
@@ -32,11 +10,14 @@ class Solution {
             if(i==n || j==n)return i==n && j==n;
             char ch1=start.charAt(i);
             char ch2=target.charAt(j);
-           if(ch1!=ch2)return false;
-           if(ch1=='L' && i<j)return false;
-           if(ch1=='R' && i>j)return false;
-           i++;
-           j++;
+        
+        if(ch1=='L' && ch2=='L'&& i>=j){
+             i++; j++; 
+         }else if(ch1=='R' && ch2=='R'&& i<=j){
+             i++; j++;
+         }else{  
+               return false;
+             }
         }
         return true;
     }
