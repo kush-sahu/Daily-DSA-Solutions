eb9227672964f9d27d30248b75ca4e3c54c09035
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2025-02-19 01:54:19 +0530

    Time: 1 ms (74.26%) | Memory: 41.7 MB (55.59%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,36 +2,32 @@ class Solution {
     public String decodeString(String s) {
         Stack<String>st=new Stack<>();
         for(int i=0;i<s.length();i++){
-           if(st.size()==0 || !s.substring(i,i+1).equals("]")){
-            st.push(s.substring(i,i+1));
-           }else if(s.substring(i,i+1).equals("]")){
-
-            String sb="";
-            while(st.size()!=0 && !st.peek().equals("[")){
-                sb=st.pop()+sb;
-            }
-            if(st.peek().equals("["))st.pop();
-            
-            if(Character.isDigit(st.peek().charAt(0))){
+            if(st.size()==0 || s.charAt(i)!=']'){
+                st.push(s.charAt(i)+"");
+            }else {
+                String sb="";
+                while(st.size()>0 && !st.peek().equals("[")){
+                  sb=st.pop()+sb;
+                }
+                if(st.size()>0 && st.peek().equals("["))st.pop();
+                if(st.size()>0 && st.peek().charAt(0)>='0' && st.peek().charAt(0)<='9'){
                 String n="";
-                while(st.size()!=0 && Character.isDigit(st.peek().charAt(0))){
-                 n=st.pop()+n;
+                while(st.size()>0 && st.peek().charAt(0)>='0' && st.peek().charAt(0)<='9'){
+                     n=st.pop()+n;
                 }
-                int num=Integer.parseInt(n);
                 String str=sb;
+                int num=Integer.parseInt(n);
                 for(int j=1;j<num;j++){
-                    sb=str+sb;
+                 sb=str+sb;
                 }
-                
+                }
+                st.push(sb);
             }
-            st.push(sb);
-           }
-           
         }
         String ans="";
-           while(st.size()>0){
+        while(st.size()!=0){
             ans=st.pop()+ans;
-           }
-           return ans;
+        }
+        return ans;
     }
 }
