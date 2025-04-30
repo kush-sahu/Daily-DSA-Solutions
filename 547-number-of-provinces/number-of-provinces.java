c4cd94552b1295d4dc84b0e9c564b567d4fa9a18
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2025-04-30 23:17:41 +0530

    Time: 7 ms (7.60%) | Memory: 46.6 MB (92.19%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -2,40 +2,38 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         List<List<Integer>>adj=new ArrayList<>();
         int n=isConnected.length;
-        for (int i = 0; i < n; i++)  adj.add(new ArrayList<>());
-        
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                if( isConnected[i][j]==1){
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-         int c=0;
-        int v[]=new int[n];
-        
-        for(int i=0;i<n;i++){
-            if(v[i]!=1){
-                c++;
-                Queue<Integer>q=new LinkedList<>();
-                q.add(i);
-                bfs(adj,v,q);
+        int ans=0;
+        boolean v[]=new boolean[n];
+        Stack<Integer>st=new Stack<>();
+        for(int i=0;i<n;i++)st.push(i);
+        while(st.size()>0){ 
+            int a=st.pop(); 
+            if(!v[a]){
+                ans++;
+                v[a]=true;
             }
+            for(int e:adj.get(a)){
+                if(!v[e]){
+                   v[e]=true;
+                   st.push(e);
+                }
+                
+            }
+           
         }
-        return c;
-
+        return ans;
     }
-    public void bfs(List<List<Integer>>adj,int v[],Queue<Integer>q){
-         while(q.size()>0){
-          int node=q.remove();
-          
-          for(int ele:adj.get(node)){
-            if(v[ele]==0){
-                q.add(ele);
-                v[ele]=1;
-            }
-          }
-         }
-    } 
 }
