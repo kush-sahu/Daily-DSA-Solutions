3f6569eb48008d489a3f3d5caa651a4b6b73eea9
Kush Sahu <121962253+kush-sahu@users.noreply.github.com>
2025-05-01 22:59:11 +0530

    Time: 509 ms (45.78%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -10,7 +10,7 @@ BEGIN
     --   limit 1 
 
 
-    select salary from (select *, ROW_NUMBER() over (order by salary desc)  as rowNum
+    select salary from (select *, dense_rank() over (order by salary desc)  as rowNum
     from Employee  group by salary) t
     where t.rowNum=N
 
